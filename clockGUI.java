04940c34a8be79c4039cd66968d4695e6ab2e98c
Isaiah Garcia <isaiahgarcia0112@hotmail.com>
2023-03-30 18:03:01 -0500

    change names and added objects

--- clockGUI.java
+++ clockGUI.java
@@ -1,3 +1,38 @@
-public class clockGUI {
-    
+import javax.swing.*;
+import javax.awt.*;
+
+public class ClockGUI 
+{
+    JFrame clockWindow = new JFrame();
+    JPanel timePanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+    JPanel logPanel = new JPanel();
+    JButton clockIn =  new JButton("Clock In");
+    JButton clockOut = new JButton("Clock Out");
+    public ClockGUI()
+    {
+        timePanel.setBackground(Color.green);
+        buttonPanel.setBackground(Color.red);
+        logPanel.setBackground(Color.yellow);
+
+        clockWindow.setLayout(new GridBagLayout());
+        GridBagConstraints whole = new GridBagConstraints();
+        whole.fill = GridBagConstraints.HORIZONTAL;
+        whole.gridx = 0;
+        whole.gridy = 0;
+        clockWindow.add(timePanel, whole);
+
+        whole.fill = GridBagConstraints.HORIZONTAL;
+        whole.gridx = 0;
+        whole.gridy = 1;
+        clockWindow.add(buttonPanel, whole);
+
+        whole.fill = GridBagConstraints.HORIZONTAL;
+        whole.gridx = 1;
+        whole.gridy = 1;
+        clockWindow.add(logPanel, whole);
+
+        clockWindow.setVisible(true);
+
+    }
 }
--- clockInOut.java
+++ clockInOut.java
@@ -1,6 +1,6 @@
-public class clockInOut {
+public class ClockInOut {
     public static void main(String[] args)
     {
-        
+        ClockGUI gui = new ClockGUI();
     }
 }
