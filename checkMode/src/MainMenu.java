b4f0618e25423f10a16eb9b65d37b90a62e927ae
RodrigoTxstate22 <rodrigo213993@gmail.com>
2023-04-02 18:19:21 -0500

    Update MainMenu.java
    first try at menu

--- checkMode/src/MainMenu.java
+++ checkMode/src/MainMenu.java
@@ -10,6 +10,9 @@ public class MainMenu {
     JPanel north = new JPanel();
     JPanel center = new JPanel();
     JTextArea test = new JTextArea();
+    static JButton b, b1, b2;
+    static JLabel l;
+    static JFrame f;
 
     public MainMenu() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,7 +24,27 @@ public class MainMenu {
         center.add(test);
 
         frame.add(center);
-
+        f = new JFrame("panel");
+        l = new JLabel("Welcome!");
+        b = new JButton("Waiter login");
+        b1 = new JButton("Manager Mode");
+        b2 = new JButton("Time clock");
+        JPanel p = new JPanel();
+        p.add(b);
+        p.add(b1);
+        p.add(b2);
+        p.add(l);
+  
+        // setbackground of panel
+        p.setBackground(Color.red);
+  
+        // Adding panel to frame
+        f.add(p);
+  
+        // Setting the size of frame
+        f.setSize(300, 300);
+  
+        f.show();
         System.out.println();
         
     }
