dcacd70cf9dd657a538243b829994b5862fcffce
RodrigoTxstate22 <rodrigo213993@gmail.com>
2023-04-02 19:59:41 -0500

    Update MainMenu.java
    button update

--- checkMode/src/MainMenu.java
+++ checkMode/src/MainMenu.java
@@ -10,7 +10,7 @@ public class MainMenu {
     JPanel north = new JPanel();
     JPanel center = new JPanel();
     JTextArea test = new JTextArea();
-    static JButton b, b1, b2;
+    static JButton b, b1, b2,b3,b4,b5;
     static JLabel l;
     static JFrame f;
 
@@ -20,29 +20,34 @@ public class MainMenu {
         frame.setLayout(new BorderLayout());
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         frame.setVisible(true);
-
+        
+  
         center.add(test);
 
         frame.add(center);
-        f = new JFrame("panel");
         l = new JLabel("Welcome!");
         b = new JButton("Waiter login");
         b1 = new JButton("Manager Mode");
         b2 = new JButton("Time clock");
+        b3 = new JButton("Register new User");
+        b4 = new JButton("Store Front");
+        b5 = new JButton("Settings");
+
+
         JPanel p = new JPanel();
+        p.add(l);
         p.add(b);
         p.add(b1);
         p.add(b2);
-        p.add(l);
+        p.add(b3);
+        p.add(b4);
+        p.add(b5);
   
         // setbackground of panel
-        p.setBackground(Color.red);
+       // p.setBackground(Color.blue);
   
         // Adding panel to frame
-        f.add(p);
-  
-        // Setting the size of frame
-        f.setSize(300, 300);
+       frame.add(p);
   
         f.show();
         System.out.println();
