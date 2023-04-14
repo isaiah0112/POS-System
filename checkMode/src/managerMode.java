a2552d102e4ab8cbb03c38e7ff74b85909242e7b
Jacob Galvez <71790883+JacobGalvez@users.noreply.github.com>
2023-04-14 01:00:47 -0500

    More Functionality in Manager Mode / menu Items are now editable

--- checkMode/src/managerMode.java
+++ checkMode/src/managerMode.java
@@ -4,20 +4,100 @@ import java.awt.event.*;
 import java.util.Arrays;
 import java.awt.Font.*;
 import java.awt.Color.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
-public class managerMode extends checkMode{
+public class managerMode {
     
     JFrame frame = new JFrame();
+    JPanel north = new JPanel();
+    JTextArea employeeName = new JTextArea();
+    JTextArea nameInput = new JTextArea();
+    JTextArea employeeID = new JTextArea();
+    JTextArea IdInput = new JTextArea();
+
     public managerMode() {
-        managerMode mgr = new managerMode();
         //edit.setVisible(false);
         frame.setTitle("Manager Mode");
-        frame.setLayout(new GridLayout(4, 1, 0, 100));
+        frame.setLayout(new BorderLayout());
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
         frame.setVisible(true);
+        
+        north.add(employeeName);
+        north.add(nameInput);
+        north.add(employeeID);
+        north.add(IdInput);
+        employeeName.setText("Employee Name: ");
+        employeeName.setEditable(false);
+        employeeID.setText("Employee ID: ");
+        employeeID.setEditable(false);
 
-        mgr.fishButton.setText("Poop");
+
+
+        frame.add(north , BorderLayout.NORTH);
+
+        modifyFile("D:/Git/POS-System/checkMode/src/menu.txt", "85", "95");
+
     }
-    
+    static void modifyFile(String filePath, String oldString, String newString)
+    {
+        File fileToBeModified = new File(filePath);
+         
+        String oldContent = "";
+         
+        BufferedReader reader = null;
+         
+        FileWriter writer = null;
+         
+        try
+        {
+            reader = new BufferedReader(new FileReader(fileToBeModified));
+             
+            //Reading all the lines of input text file into oldContent
+             
+            String line = reader.readLine();
+             
+            while (line != null) 
+            {
+                oldContent = oldContent + line + System.lineSeparator();
+                 
+                line = reader.readLine();
+            }
+             
+            //Replacing oldString with newString in the oldContent
+             
+            String newContent = oldContent.replaceAll(oldString, newString);
+             
+            //Rewriting the input text file with newContent
+             
+            writer = new FileWriter(fileToBeModified);
+             
+            writer.write(newContent);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                //Closing the resources
+                 
+                reader.close();
+                 
+                writer.close();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+ 
+
